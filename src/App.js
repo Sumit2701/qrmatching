2edbd170b0e285d936c7946fca0b6fff517dda02
Sumit2701 <sumitahire2701@gmail.com>
2024-04-01 15:01:58 +0530

    test 6

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { useState , useEffect} from 'react';
 
 function App() {
   const [arr, setArr]=useState();
-  const [str, setStr]=useState("");
+  const [str, setStr]=useState();
   const [Text, setText]=useState("");
 
   const [value, setValue]=useState();
   useEffect(() => {
+    if(str)
     setArr(str.split((" ")))
-    
   }, [str])
+
   useEffect(() => {
+    console.log(arr)
     if(value)
     setText(arr.includes(value))
     
@@ -26,11 +28,11 @@ function App() {
     onResult={(text, result) => setValue(text)}
     onError={(error) => console.log(error?.message)}
 />
-<div style={{display:"flex", justifyContent:"center" }}>
-<input onChange={(e)=>setStr(e.target.value)} ></input>
-    {value} 
+<div style={{display:"", justifyContent:"center" }}>
     {Text}
 </div>
+<textarea onChange={(e)=>setStr(e.target.value)} ></textarea>
+
 </div>
 
     </div>
