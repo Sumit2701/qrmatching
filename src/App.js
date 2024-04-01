8e471d19d9e6f75903a3803b76e8bddac7019f72
Sumit2701 <sumitahire2701@gmail.com>
2024-04-01 15:36:10 +0530

    test9

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import { useState , useEffect} from 'react';
 function App() {
   const [arr, setArr]=useState();
   const [str, setStr]=useState();
-  const [Text, setText]=useState("");
+  const [Text, setText]=useState();
 
   const [value, setValue]=useState();
   useEffect(() => {
+    console.log(Text)
     if(str)
     setArr(str.split((",")))
-  }, [str])
+  }, [str, Text])
 
   
 
@@ -20,8 +21,9 @@ const match=(text)=>
   setValue(text)
 if(text && arr){
   setText(arr.some(item => item.startsWith(text.slice(0, 20))));
- console.log(arr.some(item => item.startsWith(value.slice(0, 20))))
+ 
 }
+
 }
   
   return (
@@ -30,9 +32,10 @@ if(text && arr){
     onResult={(text, result) => match(text)}
     onError={(error) => console.log(error?.message)}
 />
+<h1>{Text}</h1> 
 <h1>{value}</h1> 
 <div style={{display:"", justifyContent:"center" }}>
-   <h1>{Text}</h1> 
+  
 </div>
 <input style={{width:"100%", height:"30px"}} onChange={(e)=>setStr(e.target.value)} ></input>
 
