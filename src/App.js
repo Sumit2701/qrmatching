e4d13811c06aefc15d2bcd2757f2570133f3dd5d
Sumit2701 <sumitahire2701@gmail.com>
2024-04-01 15:50:18 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState , useEffect} from 'react';
 function App() {
   const [arr, setArr]=useState();
   const [str, setStr]=useState();
-  const [Text, setText]=useState();
+  const [Text, setText]=useState(false);
 
   const [value, setValue]=useState();
   useEffect(() => {
@@ -17,34 +17,34 @@ function App() {
   
 
 const match=(text)=>
-{console.log(arr)
+{console.log(arr, text)
   setValue(text)
 if(text && arr){
   setText(arr.some(item => item.startsWith(text.slice(0, 20))));
- 
-}
+  console.log(arr.some(item => item.startsWith(text.slice(0, 20))));
 
 }
-  
-  return (
-    <div style={{display:"flex", justifyContent:"center" }}>
-      <div style={{width:"400px"}}>          <Scanner
-    onResult={(text, result) => match(text)}
-    onError={(error) => console.log(error?.message)}
-/>
-<h1>{Text}</h1> 
-<h1>{value}</h1> 
-<div style={{display:"", justifyContent:"center" }}>
-  
-</div>
-<input style={{width:"100%", height:"30px"}} onChange={(e)=>setStr(e.target.value)} ></input>
 
-</div>
-
+}
+  return(
+<div style={{backgroundColor: Text ? 'lightgreen' : 'lightred'}}>
+    <div style={{display:"flex", justifyContent:"center"}}>
+      <div style={{width:"400px"}}>
+        <Scanner
+          onResult={(text, result) => match(text)}
+          onError={(error) => console.log(error?.message)}
+        />
+        <h1>{Text ? <>Perfect Match</> : <>Fail</>}</h1>
+        <p>{value}</p>
+        <div style={{display:"", justifyContent:"center"}}>
+        </div>
+        <input style={{width:"100%", height:"30px"}} onChange={(e)=>setStr(e.target.value)} />
+      </div>
     </div>
+    <div style={{height:"200px"}}></div>
+  </div>
+);
 
-
-  );
 }
 
 export default App;
