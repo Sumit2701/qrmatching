6ea7b1b8e811ae87dca1f7e955a113182d4ae7de
Sumit2701 <sumitahire2701@gmail.com>
2024-04-01 15:31:08 +0530

    match method

--- src/App.js
+++ src/App.js
@@ -13,27 +13,28 @@ function App() {
     setArr(str.split((",")))
   }, [str])
 
-  useEffect(() => {
-    console.log(arr)
-    if(value && arr){
-      setText(arr.some(item => item.startsWith(value.slice(0, 20))));
-     console.log(arr.some(item => item.startsWith(value.slice(0, 20))))
-    }
-  }, [value])
+  
 
-
+const match=(text)=>
+{console.log(arr)
+  setValue(text)
+if(text && arr){
+  setText(arr.some(item => item.startsWith(text.slice(0, 20))));
+ console.log(arr.some(item => item.startsWith(value.slice(0, 20))))
+}
+}
   
   return (
     <div style={{display:"flex", justifyContent:"center" }}>
       <div style={{width:"400px"}}>          <Scanner
-    onResult={(text, result) => setValue(text)}
+    onResult={(text, result) => match(text)}
     onError={(error) => console.log(error?.message)}
 />
 <h1>{value}</h1> 
 <div style={{display:"", justifyContent:"center" }}>
    <h1>{Text}</h1> 
 </div>
-<input onChange={(e)=>setStr(e.target.value)} ></input>
+<input style={{width:"100%", height:"30px"}} onChange={(e)=>setStr(e.target.value)} ></input>
 
 </div>
 
