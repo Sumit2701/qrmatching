3fcd68c768a12dd60b75b29bea37b7bb2aef368c
Sumit2701 <sumitahire2701@gmail.com>
2024-04-01 14:30:06 +0530

    working test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import './App.css';
 import { Scanner } from '@yudiel/react-qr-scanner';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 
 function App() {
+  const [arr, setArr]=useState();
+  const [str, setStr]=useState();
+
   const [value, setValue]=useState();
+  useEffect(() => {
+    setArr(str.split((" ")))
+    
+  }, [str])
+
+  const match=(value)=>{
+   setValue( arr.includes(value))
+   // return str.includes(value)
+  }
+  
   return (
     <div style={{display:"flex", justifyContent:"center" }}>
       <div style={{width:"400px"}}>          <Scanner
-    onResult={(text, result) => setValue(text, result)}
+    onResult={(text, result) => match(text, result)}
     onError={(error) => console.log(error?.message)}
 />
 <div style={{display:"flex", justifyContent:"center" }}>
-
-    {value && <>{value} hii</>}
+<input onChange={(e)=>setStr(e.target.value)} ></input>
+    {value} 
 </div>
 </div>
 
